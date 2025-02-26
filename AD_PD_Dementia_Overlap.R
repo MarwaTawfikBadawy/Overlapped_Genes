997f281231018011614579c485e6af29d49d02ea
Marwa Tawfik Badawy <67851557+MarwaTawfikBadawy@users.noreply.github.com>
2025-02-27 00:08:35 +0200

    Update AD_PD_Dementia_Overlap.R

--- AD_PD_Dementia_Overlap.R
+++ AD_PD_Dementia_Overlap.R
@@ -3,7 +3,6 @@ library(scales)
 library(Cairo)
 library(svglite)
 
-
 # Read files
 ad_genes <- read.csv("/Users/mahamarwatawfik/Documents/AD_PD_Dementia/Alzheimer_genes.csv", header = TRUE, stringsAsFactors = FALSE)
 pd_genes <- read.csv("/Users/mahamarwatawfik/Documents/AD_PD_Dementia/Parkinson_genes.csv", header = TRUE, stringsAsFactors = FALSE)
